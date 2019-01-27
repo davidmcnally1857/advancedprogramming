a532f491d55995a7378e0d1083bfdddc1589bd38
David <davidmcnally1357@gmail.com>
2019-01-27 22:57:01 +0000

    Added folder

---
+++ CalculatorProj/CalculatorProj/Calculator.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProj
+{
+
+    public class Calculator
+    {
+       
+               
+        public Calculator()
+        {  }
+
+        public double Add(double x, double y) {
+            return x + y;
+        }
+
+        public double Subtract(double x, double y) {
+            return x - y;
+        }
+
+        public double Multiply(double x, double y) {
+            return x * y;
+        }
+
+        public double Divide(double x, double y) {
+            if (y != 0) {
+                return x / y;
+            }
+
+            else {
+                Console.WriteLine("Cannot divide by 0");
+                return -1;
+            }
+        }
+
+        public double Modulo(double x, double y) {
+            return x % y;
+        }
+
+        public double Squared(double x) {
+            return Math.Pow(x, 2);
+
+        }
+
+        public double Cubed(double x) {
+            return Math.Pow(x, 3);
+        }
+
+        public double SquareRoot(double x)
+        {
+            return Math.Sqrt(x);
+
+        }
+
+        public double Sin(double x)
+        {
+
+            return Math.Sin(x);
+           
+        }
+
+        public double Cos(double x)
+        {
+            return Math.Cos(x);
+        }
+
+        public double Tan(double x)
+        {
+            return Math.Tan(x);
+        }
+
+
+        public static void Main(string[] args) {
+
+            Calculator calculator = new Calculator();
+            Console.WriteLine("Please enter a number");
+            double x = double.Parse(Console.ReadLine().ToLower());
+            Console.WriteLine("Please enter an operation");
+            string operation = Console.ReadLine();
+            if (operation == "s" || operation == "c" || operation == "sq" || operation == "sin" || operation == "cos" || operation == "tan")
+            {
+
+                if (operation == "s")
+                {
+                    Console.WriteLine(calculator.Squared(x));
+                }
+                else if (operation == "c")
+                {
+                   Console.WriteLine(calculator.Cubed(x));
+                }
+                else if(operation == "sq")
+                {
+                    Console.WriteLine(calculator.SquareRoot(x));
+                }
+
+                else if(operation == "sin")
+                {
+                    Console.WriteLine(calculator.Sin(x));
+                }
+
+                else if(operation == "cos")
+                {
+                    Console.WriteLine(calculator.Cos(x));
+                }
+
+                else if(operation == "tan")
+                {
+                    Console.WriteLine(calculator.Tan(x));
+                }
+
+            }
+            else {
+
+                Console.WriteLine("Please enter a number");
+                double y = int.Parse(Console.ReadLine().ToLower());
+                switch (operation)
+                {
+                    case "+":
+                        Console.WriteLine(calculator.Add(x, y));
+                        break;
+                    case "-":
+                        Console.WriteLine(calculator.Subtract(x, y));
+                        break;
+                    case "*":
+                        Console.WriteLine(calculator.Multiply(x, y));
+                        break;
+                    case "/":
+                        Console.WriteLine(calculator.Divide(x, y));
+                        break;
+                    case "%":
+                        Console.WriteLine(calculator.Modulo(x, y));
+                        break;
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+
+                }
+            }
+            Console.ReadLine();
+
+        }
+        
+            
+           
+        }
+    }
+
---
+++ CalculatorProj/CalculatorProjTests/ProgramTests.cs
@@ -0,0 +1,126 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CalculatorProj;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CalculatorProj.Tests
+{
+    [TestClass()]
+    public class ProgramTests {
+
+        public double x;
+        public double y;
+
+        [TestMethod()]
+        public void Adding3plus3ShouldGive6() {
+
+            Calculator calculator = new Calculator();
+             x = 3;
+             y = 3;
+            Assert.AreEqual(6, calculator.Add(x, y));
+        }
+
+        [TestMethod()]
+        public void Subtract18by3ShouldGive15() {
+            Calculator calculator = new Calculator();
+             x = 18;
+             y = 3;
+            Assert.AreEqual(15, calculator.Subtract(x, y));
+        }
+
+        [TestMethod()]
+        public void Multplying7by8ShouldGive56() {
+            Calculator calculator = new Calculator();
+             x = 7;
+             y = 8;
+            Assert.AreEqual(56, calculator.Multiply(x, y));
+        }
+
+        [TestMethod()]
+        public void Divide120by10SHouldGive12() {
+            Calculator calculator = new Calculator();
+             x = 120;
+             y = 10;
+            Assert.AreEqual(12, calculator.Divide(x, y));
+         }
+
+        [TestMethod()]
+        public void DivideBYZeroShouldGiveAnError() {
+            Calculator calculator = new Calculator();
+             x = 12;
+             y = 0;
+            Assert.AreEqual(-1, calculator.Divide(x, y));
+        }
+
+        [TestMethod()]
+        public void Modulo50by3ShouldGive2() {
+            Calculator calculator = new Calculator();
+             x = 50;
+             y = 3;
+            Assert.AreEqual(2, calculator.Modulo(x, y));
+        }
+
+
+        [TestMethod()]
+        public void SquaredBy10ShoulsGive100()
+        {
+            Calculator calculator = new Calculator();
+             x = 10;
+            Assert.AreEqual(100, calculator.Squared(x));
+        }
+
+        [TestMethod()]
+        public void CubedBy10ShoulsGive1000()
+        {
+            Calculator calculator = new Calculator();
+             x = 10;
+            Assert.AreEqual(1000, calculator.Cubed(x));
+        }
+
+        [TestMethod()]
+        public void SquareRootOf25SHouldGive5()
+        {
+            Calculator calculator = new Calculator();
+             x = 25;
+            Assert.AreEqual(5, calculator.SquareRoot(x));
+        }
+
+        [TestMethod()]
+        public void CalculateSin()
+        {
+            Calculator calculator = new Calculator();
+            x = 30;
+            Assert.AreEqual(-0.988031624092862, calculator.Sin(x));
+
+        }
+
+        [TestMethod()]
+        public void CalculateCos()
+        {
+            Calculator calculator = new Calculator();
+            x = 40;
+            Assert.AreEqual(-0.666938061652262, calculator.Cos(x));
+            
+
+        }
+
+
+
+        [TestMethod()]
+        public void CalculateTan()
+        {
+            Calculator calculator = new Calculator();
+            x = 20;
+            Assert.AreEqual(2.23716094422474, calculator.Tan(x));
+
+        }
+
+
+
+
+       
+    }
+}
