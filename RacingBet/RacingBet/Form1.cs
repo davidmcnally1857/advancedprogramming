1e1c3ba217bdff1f3f025d7e7b13249464525676
David <davidmcnally1357@gmail.com>
2019-02-11 20:06:28 +0000

    added folder

---
+++ RacingBet/RacingBet/Form1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace RacingBet
+{
+    public partial class frmRacingBet : Form
+    {
+        public frmRacingBet()
+        {
+            InitializeComponent();
+        }
+
+        private void frmRacingBet_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ RacingBet/RacingBetTests/frmRacingBetTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RacingBet;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace RacingBet.Tests
+{
+    [TestClass()]
+    public class frmRacingBetTests
+    {
+        [TestMethod()]
+        public void frmRacingBetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CheckIfFileHasValue()
+        {
+
+        }
+    }
+}
